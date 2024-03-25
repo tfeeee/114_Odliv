27caaf472180fc51ff0d9ba4336ed7d35cf197d2
tfecura@gmail.com <tfeeee>
2024-03-25 11:34:31 +0100

    Simple TCP client added

--- MainForm.cpp
+++ MainForm.cpp
@@ -14,3 +14,37 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
 }
 //---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button1Click(TObject *Sender)
+{
+	Client->Connect(L"192.168.144.102");
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button2Click(TObject *Sender)
+{
+	unsigned char A[2] = {1, 0};
+	A[0] = 1;
+	A[1] = 0;
+	Client->Socket->Write(A, 2);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button3Click(TObject *Sender)
+{
+	unsigned char A[2];
+	A[0] = 2;
+	A[1] = (unsigned char) StrToInt(Edit1->Text);
+	Client->Socket->Write(A, 2);
+}
+//---------------------------------------------------------------------------
+
+void __fastcall TForm1::Button4Click(TObject *Sender)
+{
+	unsigned char A[2] = {3, 0};
+	A[0] = 3;
+	A[1] = 0;
+	Client->Socket->Write(A, 2);
+}
+//---------------------------------------------------------------------------
+
--- MainForm.h
+++ MainForm.h
@@ -12,6 +12,19 @@
 #include <FMX.Types.hpp>
 #include <FMX.Layouts.hpp>
 #include <FMX.TreeView.hpp>
+#include <IdBaseComponent.hpp>
+#include <IdComponent.hpp>
+#include <IdCustomTCPServer.hpp>
+#include <IdTCPClient.hpp>
+#include <IdTCPConnection.hpp>
+#include <IdTCPServer.hpp>
+#include <FMX.Memo.hpp>
+#include <FMX.Memo.Types.hpp>
+#include <FMX.ScrollBox.hpp>
+#include <IdUDPBase.hpp>
+#include <IdUDPClient.hpp>
+#include <IdUDPServer.hpp>
+#include <FMX.Edit.hpp>
 //---------------------------------------------------------------------------
 class TForm1 : public TForm
 {
@@ -20,7 +33,18 @@ __published:	// IDE-managed Components
 	TStatusBar *SB1;
 	TTabItem *MainTab;
 	TTabItem *SetupTab;
-	TTreeView *TreeView1;
+	TTabItem *TabItem1;
+	TMemo *Memo1;
+	TIdTCPClient *Client;
+	TButton *Button1;
+	TButton *Button2;
+	TButton *Button3;
+	TEdit *Edit1;
+	TButton *Button4;
+	void __fastcall Button1Click(TObject *Sender);
+	void __fastcall Button2Click(TObject *Sender);
+	void __fastcall Button3Click(TObject *Sender);
+	void __fastcall Button4Click(TObject *Sender);
 private:	// User declarations
 public:		// User declarations
 	__fastcall TForm1(TComponent* Owner);
